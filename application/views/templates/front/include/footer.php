15655e35329a0f031b826144312b1b06b2a74de6
x404 <kronos2003@gmail.com>
2017-05-31 22:47:12 +0300

    Адаптация каталога. Отключены вк, твиттер, г+

--- application/views/templates/front/include/footer.php
+++ application/views/templates/front/include/footer.php
@@ -54,7 +54,7 @@
 </div>
 
 
-
+<!--
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 <script type="text/javascript">
       window.___gcfg = {
@@ -67,6 +67,8 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 </script>
+
+-->
 
 </body>
 </html>
